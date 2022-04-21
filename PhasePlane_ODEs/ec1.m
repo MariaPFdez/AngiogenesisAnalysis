fb8c6c50eadfdb4eda4cff67f3ce21e0518f65f4
María Peña Fernández <102073770+MariaPFdez@users.noreply.github.com>
2022-04-21 13:45:34 +0200

    Add files via upload

--- PhasePlane_ODEs/ec1.m
+++ PhasePlane_ODEs/ec1.m
@@ -12,45 +12,48 @@ d=0.00873; %pto critico del artículo, V=K=17346.52
 %e=0.66;%para el fármaco Endostatina
 e=0.15;%para el fármaco Angiostatina
 
-% %% ODE45 CON EL SISTEMA DE ECUACIONES
-% %condiciones iniciales en el vector x0=[v0,k0]
-% x0=[180,625];%[V0,K0]
-% tf=200; %tiempo final, tf
-% f=@(t,x) [-lam1*x(1)*log(x(1)/x(2));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)];
-% [t,x]=ode45(f,[0,tf],x0);
-% plot(t,x)
-% grid on
-% xlabel('t')
-% ylabel('V,K');
-% title('Volumen y capacidad de carga en función del tiempo, sin fármaco')
-% fig1=figure;
-% %plot(x(:,1),x(:,2))
-% %hold on
-
-%% ODE45 CON EL SISTEMA DE ECUACIONES CON FÁRMACO
+%% ODE45 CON EL SISTEMA DE ECUACIONES
 %condiciones iniciales en el vector x0=[v0,k0]
-x0=[180,625];
-tf=140; %tiempo final, tf
-%f=@(t,x) [-lam1*x(1)*log(x(1)/x(2))*(1-ec1c(t)*0.7/(0.8+ec1c(t)));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)];%citotóxico
-%f=@(t,x) [-lam1*x(1)*log(x(1)/x(2))*(1-30.74*0.7/(0.8+30.74));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)]
-f=@(t,x) [-lam1*x(1)*log(x(1)/x(2));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*ec1c(t)];%antiangiogénico
-[t1,x1]=ode45(f,[0,tf],x0);
-f=@(t,x) [-lam1*x(1)*log(x(1)/x(2));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*15.32];
-%f=@(t,x) [-lam1*x(1)*log(x(1)/x(2))*(1-ec1c(t)*1.2/(0.8+ec1c(t)));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*ec1c(t)];%ambos
+x0=[180,625];%[V0,K0]
+tf=2000; %tiempo final, tf
+f=@(t,x) [-lam1*x(1)*log(x(1)/x(2));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*ec1c(t)];
 [t,x]=ode45(f,[0,tf],x0);
-f=@(t,x) [-lam1*x(1)*log(x(1)/x(2));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*25.32];
-[t2,x2]=ode45(f,[0,tf],x0);
-x(length(x),1)
-plot(t,x(:,1))
-hold on
-plot(t1,x1(:,1))
-hold on
-plot(t2,x2(:,1))
-xlabel('\it{t}')
-ylabel(['\it{V}']);
-title('Volume (\it{V}) \rm{when c_{min}, c_{max} and c(t) if Treatment 2 is applied.}')
-legend('c_{min}','c(t)','c_{max}')
+plot(t,x(:,2),'r')
+hold on 
+grid off
+plot(t,x(:,1),'b')
+xlabel('\it{t} \rm{(days)}')
+ylabel('\it{V, K} \rm{(mm^3)}');
+legend('K','V')
+title('\it{V} \rm{y} \it{K} \rm{en función del tiempo, con dosis de} \it{d_i}= \rm{20 mg/kg}')
 fig1=figure;
+% plot(x(:,1),x(:,2))
+% hold on
+
+% %% ODE45 CON EL SISTEMA DE ECUACIONES CON FÁRMACO
+% %condiciones iniciales en el vector x0=[v0,k0]
+% x0=[180,625];
+% tf=5000; %tiempo final, tf
+% %f=@(t,x) [-lam1*x(1)*log(x(1)/x(2))*(1-ec1c(t)*0.7/(0.8+ec1c(t)));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)];%citotóxico
+% %f=@(t,x) [-lam1*x(1)*log(x(1)/x(2))*(1-30.74*0.7/(0.8+30.74));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)]
+% f=@(t,x) [-lam1*x(1)*log(x(1)/x(2));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*ec1c(t)];%antiangiogénico
+% [t1,x1]=ode45(f,[0,tf],x0);
+% f=@(t,x) [-lam1*x(1)*log(x(1)/x(2));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*15.32];
+% %f=@(t,x) [-lam1*x(1)*log(x(1)/x(2))*(1-ec1c(t)*1.2/(0.8+ec1c(t)));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*ec1c(t)];%ambos
+% [t,x]=ode45(f,[0,tf],x0);
+% f=@(t,x) [-lam1*x(1)*log(x(1)/x(2));-lam2*x(2)+b*x(1)-d*x(2)*x(1)^(2/3)-e*x(2)*25.32];
+% [t2,x2]=ode45(f,[0,tf],x0);
+% x(length(x),1)
+% plot(t,x(:,1))
+% hold on
+% plot(t1,x1(:,1))
+% hold on
+% plot(t2,x2(:,1))
+% xlabel('\it{t}')
+% ylabel(['\it{V}']);
+% title('Volume (\it{V}) \rm{when c_{min}, c_{max} and c(t) if Treatment 2 is applied.}')
+% legend('c_{min}','c(t)','c_{max}')
+% fig1=figure;
 
 % %% ODE45 CON EL SISTEMA AUTÓNOMO SIN FÁRMACO
 % %condiciones iniciales del volumen en el vector x0
@@ -68,20 +71,4 @@ fig1=figure;
 % %por el carácter asintótico del punto crítico la estabilidad NO se alcanza
 % %a partir de Ve=Ke (17000 aprox)
 
-% %% PLANO DE FASES (queda mejor con el paquete PhasePlane)
-% %tamaño de la gráfica
-% y1 = linspace(1,20,20);
-% y2 = linspace(1,20,20);
-% [xx,yy] = meshgrid(y1,y2);
-% u = ones(size(xx));
-% v = ones(size(xx));
-% t=0;
-% for i = 1:numel(xx)
-%     Yprime = f(t,[xx(i); yy(i)]);
-%     u(i) = Yprime(1);
-%     v(i) = Yprime(2);
-% end
-% quiver(xx,yy,u,v,'r'); figure(gcf)
-% title('Plano de fases del sistema')
-% hold on
-% plot(y,x)
+
